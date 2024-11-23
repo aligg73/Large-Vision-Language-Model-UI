998f976a6d668802665e2d386df18a2a0e892491
Alex Greve <me@alexgreve.com>
2024-11-23 16:54:20 +0100

    Adding bash scripts

---
+++ initGradioUI.sh
@@ -0,0 +1,14 @@
+echo "is CUDA 12.4 being used?"
+python -V
+nvidia-smi
+# git clone https://github.com/Kaszebe/Large-Vision-Language-Model-UI.git
+# cd Large-Vision-Language-Model-UI
+# python3 -m venv qwen_venv
+source qwen_venv/bin/activate
+pip install --upgrade pip
+pip install wheel
+pip install transformers accelerate qwen-vl-utils gradio autoawq
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+pip install flash-attn --no-build-isolation
+export QWEN_MODEL_PATH="/workspace/Large-Vision-Language-Model-UI/model"
+python run_qwen_model.py --flash-attn2
